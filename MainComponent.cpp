8e21f1aa69e6a882284a85c4d536cbff74c1ff06
Victoria Grace <victoriagrace@DN51t4bv.SUNet>
2016-10-02 21:34:47 -0700

    omg

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -9,6 +9,7 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "Sine.h"
+#define NUMSLIDERS 4
 
 class SineComponents
 {
@@ -39,46 +40,50 @@ class MainContentComponent :
 public:
     MainContentComponent() : samplingRate(0.0)
     {
+        for (int n=0; n < NUMSLIDERS; n++) {
+            
+        
         // configuring frequency slider and adding it to the main window
-        addAndMakeVisible (sc[0].frequencySlider);
-        sc[0].frequencySlider.setRange (50.0, 5000.0);
-        sc[0].frequencySlider.setSkewFactorFromMidPoint (500.0);
-        sc[0].frequencySlider.setValue(1000); // will also set the default frequency of the sine osc
-        sc[0].frequencySlider.addListener (this);
+        addAndMakeVisible (sc[n].frequencySlider);
+        sc[n].frequencySlider.setRange (50.0, 5000.0);
+        sc[n].frequencySlider.setSkewFactorFromMidPoint (500.0);
+        sc[n].frequencySlider.setValue(1000); // will also set the default frequency of the sine osc
+        sc[n].frequencySlider.addListener (this);
         
         // configuring frequency label box and adding it to the main window
-        addAndMakeVisible(sc[0].frequencyLabel);
-        sc[0].frequencyLabel.setText ("Frequency", dontSendNotification);
-        sc[0].frequencyLabel.attachToComponent (&sc[0].frequencySlider, true);
+        addAndMakeVisible(sc[n].frequencyLabel);
+        sc[n].frequencyLabel.setText ("Frequency", dontSendNotification);
+        sc[n].frequencyLabel.attachToComponent (&sc[n].frequencySlider, true);
         
         
         // configuring gain slider and adding it to the main window
-        addAndMakeVisible (sc[0].gainSlider);
-        sc[0].gainSlider.setRange (0.0, 1.0);
-        sc[0].gainSlider.setValue(0.5); // will alsi set the default gain of the sine osc
-        sc[0].gainSlider.addListener (this);
+        addAndMakeVisible (sc[n].gainSlider);
+        sc[n].gainSlider.setRange (0.0, 1.0);
+        sc[n].gainSlider.setValue(0.5); // will alsi set the default gain of the sine osc
+        sc[n].gainSlider.addListener (this);
         
         
         // configuring gain label and adding it to the main window
-        addAndMakeVisible(sc[0].gainLabel);
-        sc[0].gainLabel.setText ("Gain", dontSendNotification);
-        sc[0].gainLabel.attachToComponent (&sc[0].gainSlider, true);
+        addAndMakeVisible(sc[n].gainLabel);
+        sc[n].gainLabel.setText ("Gain", dontSendNotification);
+        sc[n].gainLabel.attachToComponent (&sc[n].gainSlider, true);
         
         
         // configuring on/off button and adding it to the main window
-        addAndMakeVisible(sc[0].onOffButton);
-        sc[0].onOffButton.addListener(this);
+        addAndMakeVisible(sc[n].onOffButton);
+        sc[n].onOffButton.addListener(this);
         
         
         // configuring on/off label and adding it to the main window
-        addAndMakeVisible(sc[0].onOffLabel);
-        sc[0].onOffLabel.setText ("On/Off", dontSendNotification);
-        sc[0].onOffLabel.attachToComponent (&sc[0].onOffButton, true);
-        
-        setSize (600, 100);
+        addAndMakeVisible(sc[n].onOffLabel);
+        sc[n].onOffLabel.setText ("On/Off", dontSendNotification);
+        sc[n].onOffLabel.attachToComponent (&sc[n].onOffButton, true);
+        }
+        setSize (600, 100 * NUMSLIDERS);
         nChans = 1;
         setAudioChannels (0, nChans); // no inputs, one output
     }
+    
     
     ~MainContentComponent()
     {
@@ -90,38 +95,53 @@ public:
         // placing the UI elements in the main window
         // getWidth has to be used in case the window is resized by the user
         const int sliderLeft = 80;
-        sc[0].frequencySlider.setBounds (sliderLeft, 10, getWidth() - sliderLeft - 20, 20);
-        sc[0].gainSlider.setBounds (sliderLeft, 40, getWidth() - sliderLeft - 20, 20);
-        sc[0].onOffButton.setBounds (sliderLeft, 70, getWidth() - sliderLeft - 20, 20);
+        for(int n = 0; n < NUMSLIDERS; n++) {
+            sc[n].frequencySlider.setBounds (sliderLeft, 10 + 90 * n, getWidth() - sliderLeft - 20, 20);
+            sc[n].gainSlider.setBounds (sliderLeft, 40 + 90 * n, getWidth() - sliderLeft - 20, 20);
+            sc[n].onOffButton.setBounds (sliderLeft, 70 + 90 * n, getWidth() - sliderLeft - 20, 20);
+        }
     }
     
     void sliderValueChanged (Slider* slider) override
     {
-        if (samplingRate > 0.0){
-            if (slider == &sc[0].frequencySlider){
-                sc[0].sine.setFrequency(sc[0].frequencySlider.getValue());
-            }
-            else if (slider == &sc[0].gainSlider){
-                sc[0].gain = sc[0].gainSlider.getValue();
+        for(int n = 0; n < NUMSLIDERS; n++) {
+
+            if (samplingRate > 0.0){
+                if (slider == &sc[n].frequencySlider){
+                    sc[n].sine.setFrequency(sc[n].frequencySlider.getValue());
+                    break;
+                }
+                else if (slider == &sc[n].gainSlider){
+                    sc[n].gain = sc[n].gainSlider.getValue();
+                    break;
+                }
             }
         }
     }
     
     void buttonClicked (Button* button) override
     {
+        for(int n = 0; n < NUMSLIDERS; n++) {
+
         // turns audio on or off
-        if(button == &sc[0].onOffButton && sc[0].onOffButton.getToggleState()){
-            sc[0].onOff = 1;
-        }
-        else{
-            sc[0].onOff = 0;
+            if(button == &sc[n].onOffButton && sc[n].onOffButton.getToggleState()){
+                sc[n].onOff = 1;
+                break;
+            }
+            else if(button == &sc[n].onOffButton) {
+                sc[n].onOff = 0;
+                break;
+            }
         }
     }
     
     void prepareToPlay (int /*samplesPerBlockExpected*/, double sampleRate) override
     {
+        for(int n = 0; n < NUMSLIDERS; n++) {
+
         samplingRate = sampleRate;
-        sc[0].sine.setSamplingRate(sampleRate);
+        sc[n].sine.setSamplingRate(sampleRate);
+        }
     }
     
     void releaseResources() override
@@ -136,18 +156,17 @@ public:
         // computing one block
         for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
         {
-            if(sc[0].onOff == 1) buffer[sample] = sc[0].sine.tick() * sc[0].gain;
-            
-           // if(onOff == 1) buffer[sample] = (float) rand()/RAND_MAX * gain;
-
-            else buffer[sample] = 0.0;
+            for(int n = 0; n < NUMSLIDERS; n++) {
+                if(sc[n].onOff == 1) buffer[sample] += sc[n].sine.tick() * sc[n].gain;
+                else buffer[sample] += 0.0;
+            }
         }
     }
     
     
 private:
 
-    SineComponents sc[1];
+    SineComponents sc[NUMSLIDERS];
 
     int samplingRate, nChans;
     
